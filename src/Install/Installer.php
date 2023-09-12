9759e9a7b72a311b488c9ccdd7dd08cb89fe74f1
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-09-12 20:01:55 +0100

    update installer

--- src/Install/Installer.php
+++ src/Install/Installer.php
@@ -2,10 +2,20 @@
 
 namespace Luma\Framework\Install;
 
+namespace Luma\Framework\Install;
+
 class Installer
 {
     public static function install(): void
     {
-        var_dump(dirname(__DIR__));
+        $composerJsonPath = sprintf('%s/composer.json', dirname(__DIR__, 5));
+        $composerText = file_get_contents($composerJsonPath);
+        $composerJson = json_decode($composerText);
+
+        $composerText = str_replace($composerJson->name, 'vendor/luma-project', $composerText);
+        $composerText = str_replace($composerJson->authors[0]->name, 'Your Name', $composerText);
+        $composerText = str_replace($composerJson->authors[0]->email, 'yourname@email.com', $composerText);
+
+        file_put_contents($composerJsonPath, $composerText);
     }
 }
