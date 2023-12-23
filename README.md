ad978a050832c9226b76a73fbc17623679e9c12f
Daniel Winning <daniel@winningwebdesign.co.uk>
2023-12-23 16:32:21 +0000

    update installer to build assets

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 <div>
 <!-- Version Badge -->
-<img src="https://img.shields.io/badge/Version-0.2.7-blue" alt="Version 0.2.7">
+<img src="https://img.shields.io/badge/Version-0.3.0-blue" alt="Version 0.3.0">
 <!-- PHP Coverage Badge -->
-<img src="https://img.shields.io/badge/PHP Coverage-29.17%25-red" alt="PHP Coverage 29.17%">
+<img src="https://img.shields.io/badge/PHP Coverage-24.56%25-red" alt="PHP Coverage 24.56%">
 <!-- License Badge -->
 <img src="https://img.shields.io/badge/License-GPL--3.0--or--later-34ad9b" alt="License GPL--3.0--or--later">
 </div>
--- src/Install/Installer.php
+++ src/Install/Installer.php
@@ -6,15 +6,34 @@ namespace Luma\Framework\Install;
 
 class Installer
 {
+    /**
+     * @return void
+     */
     public static function install(): void
     {
-        $gitignorePath = sprintf('%s/.gitignore', dirname(__DIR__, 5));
+        self::updateGitignore();
+        self::updateComposerJson();
+        self::installAssets();
+    }
+
+    /**
+     * @return void
+     */
+    private static function updateGitignore(): void
+    {
+        $gitignorePath = sprintf('%s/.gitignore', self::projectDirectory());
         $gitignoreContent = file_get_contents($gitignorePath);
         $gitignoreContent = str_replace('/public/assets/*', '', $gitignoreContent);
 
         file_put_contents($gitignorePath, $gitignoreContent);
+    }
 
-        $composerJsonPath = sprintf('%s/composer.json', dirname(__DIR__, 5));
+    /**
+     * @return void
+     */
+    private static function updateComposerJson(): void
+    {
+        $composerJsonPath = sprintf('%s/composer.json', self::projectDirectory());
         $composerText = file_get_contents($composerJsonPath);
         $composerJson = json_decode($composerText);
 
@@ -23,5 +42,21 @@ class Installer
         $composerText = str_replace($composerJson->authors[0]->email, 'yourname@email.com', $composerText);
 
         file_put_contents($composerJsonPath, $composerText);
+    }
+
+    /**
+     * @return void
+     */
+    private static function installAssets(): void
+    {
+        exec(sprintf('cd %s && npm install && npm run build', self::projectDirectory()));
+    }
+
+    /**
+     * @return string
+     */
+    private static function projectDirectory(): string
+    {
+        return dirname(__DIR__, 5);
     }
 }
