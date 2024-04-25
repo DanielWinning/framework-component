32b44ad771e63ff4749947358c4e1873be764d83
Daniel Winning <DanielWinning@users.noreply.github.com>
2024-04-25 18:16:19 +0100

    Add constants to flash message class

--- src/Messages/FlashMessage.php
+++ src/Messages/FlashMessage.php
@@ -6,6 +6,12 @@ class FlashMessage
 {
     private string $message;
 
+    public const INFO = 'info';
+    public const ERROR = 'error';
+    public const SUCCESS = 'success';
+    public const NOTICE = 'notice';
+    public const WARNING = 'warning';
+
     public function __construct(string $message)
     {
         $this->message = $message;
